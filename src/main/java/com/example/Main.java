23ac26def5d0b19c0b712978d95dff2d5d9e3c99
rikitsus <rikitsus@fsi.co.jp>
2018-10-24 14:24:53 +0900

    test

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,88 +1,82 @@
-/*
- * Copyright 2002-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package sfdc_rest;
 
-package com.example;
+import java.io.IOException;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.client.ClientProtocolException;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import org.json.JSONException;
 
-@Controller
-@SpringBootApplication
 public class Main {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
+    static final String USERNAME     = "rikitsus@fsi.co.jp";
+    static final String PASSWORD     = "aimi9029";
+    static final String LOGINURL     = "https://login.salesforce.com";
+    static final String GRANTSERVICE = "/services/oauth2/token?grant_type=password";
+    static final String CLIENTID     = "3MVG9d8..z.hDcPJdLBa.M9Z7BCC.6owuDjFWJqZf6Z10FYMKIXW6HaX8feL0iNojerSpzR.WdpCKneCPZgXA";
+    static final String CLIENTSECRET = "1505113103918214908";
 
-  @Autowired
-  private DataSource dataSource;
+    public static void main(String[] args) {
 
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Main.class, args);
-  }
+        DefaultHttpClient httpclient = new DefaultHttpClient();
 
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
+        // Assemble the login request URL
+        String loginURL = LOGINURL + 
+                          GRANTSERVICE + 
+                          "&client_id=" + CLIENTID + 
+                          "&client_secret=" + CLIENTSECRET +
+                          "&username=" + USERNAME +
+                          "&password=" + PASSWORD;
 
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+        // Login requests must be POSTs
+        HttpPost httpPost = new HttpPost(loginURL);
+        HttpResponse response = null;
 
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
+        try {
+            // Execute the login POST request
+            response = httpclient.execute(httpPost);
+        } catch (ClientProtocolException cpException) {
+            // Handle protocol exception
+        } catch (IOException ioException) {
+            // Handle system IO exception
+        }
 
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
+        // verify response is HTTP OK
+        final int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode != HttpStatus.SC_OK) {
+            System.out.println("Error authenticating to Force.com: "+statusCode);
+            // Error is in EntityUtils.toString(response.getEntity()) 
+            return;
+        }
+
+        String getResult = null;
+        try {
+            getResult = EntityUtils.toString(response.getEntity());
+        } catch (IOException ioException) {
+            // Handle system IO exception
+        }
+        JSONObject jsonObject = null;
+        String loginAccessToken = null;
+        String loginInstanceUrl = null;
+        try {
+            jsonObject = (JSONObject) new JSONTokener(getResult).nextValue();
+            loginAccessToken = jsonObject.getString("access_token");
+            loginInstanceUrl = jsonObject.getString("instance_url");
+        } catch (JSONException jsonException) {
+            // Handle JSON exception
+        }
+        System.out.println(response.getStatusLine());
+        System.out.println("Successful login");
+        System.out.println("  instance URL: "+loginInstanceUrl);
+        System.out.println("  access token/session ID: "+loginAccessToken);
+
+        // release connection
+        httpPost.releaseConnection();
     }
-  }
-
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-    }
-  }
-
 }
