323e87be30a0f5474f9ec7693ebfd6b1fbf2d080
rikitsus <rikitsus@fsi.co.jp>
2018-10-24 15:13:27 +0900

    test

--- README.md
+++ README.md
@@ -1,42 +1,14 @@
-# java-getting-started
+# JAX-RS Template Application
 
-[![CircleCI](https://circleci.com/gh/heroku/java-getting-started.svg?style=svg)](https://circleci.com/gh/heroku/java-getting-started)
+This is a template for a lightweight RESTful API using JAX-RS. The sample code is a call for getting the current time.
+    
+## Running the application locally
 
-A barebones Java app, which can easily be deployed to Heroku.
+First build with:
 
-This application supports the [Getting Started with Java on Heroku](https://devcenter.heroku.com/articles/getting-started-with-java) article - check it out.
+    $mvn clean install
 
-[![Deploy to Heroku](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+Then run it with:
 
-## Running Locally
+    $ java -cp target/classes:target/dependency/* com.example.Main
 
-Make sure you have Java and Maven installed.  Also, install the [Heroku CLI](https://cli.heroku.com/).
-
-```sh
-$ git clone https://github.com/heroku/java-getting-started.git
-$ cd java-getting-started
-$ mvn install
-$ heroku local:start
-```
-
-Your app should now be running on [localhost:5000](http://localhost:5000/).
-
-If you're going to use a database, ensure you have a local `.env` file that reads something like this:
-
-```
-DATABASE_URL=postgres://localhost:5432/java_database_name
-```
-
-## Deploying to Heroku
-
-```sh
-$ heroku create
-$ git push heroku master
-$ heroku open
-```
-
-## Documentation
-
-For more information about using Java on Heroku, see these Dev Center articles:
-
-- [Java on Heroku](https://devcenter.heroku.com/categories/java)
--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,171 +1,48 @@
-
-
-/*
- * Copyright 2002-2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example;
 
-package sfdc_rest;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
 
-import java.io.IOException;
-
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.util.EntityUtils;
-import org.apache.http.client.ClientProtocolException;
-
-import org.json.JSONObject;
-import org.json.JSONTokener;
-import org.json.JSONException;
-
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Map;
-
-@Controller
-@SpringBootApplication
+/**
+ *
+ * This class launches the web application in an embedded Jetty container.
+ * This is the entry point to your application. The Java command that is used for
+ * launching should fire this main method.
+ *
+ */
 public class Main {
 
-  @Value("${spring.datasource.url}")
-  private String dbUrl;
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception{
+        String webappDirLocation = "src/main/webapp/";
 
-  @Autowired
-  private DataSource dataSource;
-
-  public static void main(String[] args) throws Exception {
-    SpringApplication.run(Main.class, args);
-  }
-
-  @RequestMapping("/")
-  String index() {
-    return "index";
-  }
-
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-  	static final String USERNAME     = "rikitsus@fsi.co.jp";
-    static final String PASSWORD     = "aimi9029";
-    static final String LOGINURL     = "https://login.salesforce.com";
-    static final String GRANTSERVICE = "/services/oauth2/token?grant_type=password";
-    static final String CLIENTID     = "3MVG9d8..z.hDcPJdLBa.M9Z7BCC.6owuDjFWJqZf6Z10FYMKIXW6HaX8feL0iNojerSpzR.WdpCKneCPZgXA";
-    static final String CLIENTSECRET = "1505113103918214908";
-
-
-        DefaultHttpClient httpclient = new DefaultHttpClient();
-
-        // Assemble the login request URL
-        String loginURL = LOGINURL + 
-                          GRANTSERVICE + 
-                          "&client_id=" + CLIENTID + 
-                          "&client_secret=" + CLIENTSECRET +
-                          "&username=" + USERNAME +
-                          "&password=" + PASSWORD;
-
-        // Login requests must be POSTs
-        HttpPost httpPost = new HttpPost(loginURL);
-        HttpResponse response = null;
-
-        try {
-            // Execute the login POST request
-            response = httpclient.execute(httpPost);
-        } catch (ClientProtocolException cpException) {
-            // Handle protocol exception
-        } catch (IOException ioException) {
-            // Handle system IO exception
+        // The port that we should run on can be set into an environment variable
+        // Look for that variable and default to 8080 if it isn't there.
+        String webPort = System.getenv("PORT");
+        if (webPort == null || webPort.isEmpty()) {
+            webPort = "8080";
         }
 
-        // verify response is HTTP OK
-        final int statusCode = response.getStatusLine().getStatusCode();
-        if (statusCode != HttpStatus.SC_OK) {
-            System.out.println("Error authenticating to Force.com: "+statusCode);
-            // Error is in EntityUtils.toString(response.getEntity()) 
-            return;
-        }
+        Server server = new Server(Integer.valueOf(webPort));
+        WebAppContext root = new WebAppContext();
 
-        String getResult = null;
-        try {
-            getResult = EntityUtils.toString(response.getEntity());
-        } catch (IOException ioException) {
-            // Handle system IO exception
-        }
-        JSONObject jsonObject = null;
-        String loginAccessToken = null;
-        String loginInstanceUrl = null;
-        try {
-            jsonObject = (JSONObject) new JSONTokener(getResult).nextValue();
-            loginAccessToken = jsonObject.getString("access_token");
-            loginInstanceUrl = jsonObject.getString("instance_url");
-        } catch (JSONException jsonException) {
-            // Handle JSON exception
-        }
-        System.out.println(response.getStatusLine());
-        System.out.println("Successful login");
-        System.out.println("  instance URL: "+loginInstanceUrl);
-        System.out.println("  access token/session ID: "+loginAccessToken);
+        root.setContextPath("/");
+        root.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+        root.setResourceBase(webappDirLocation);
 
-        // release connection
-        httpPost.releaseConnection();
-  	
-  	
-  	
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+        // Parent loader priority is a class loader setting that Jetty accepts.
+        // By default Jetty will behave like most web containers in that it will
+        // allow your application to replace non-server libraries that are part of the
+        // container. Setting parent loader priority to true changes this behavior.
+        // Read more here: http://wiki.eclipse.org/Jetty/Reference/Jetty_Classloading
+        root.setParentLoaderPriority(true);
 
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
+        server.setHandler(root);
 
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
+        server.start();
+        server.join();
     }
-  }
-
-  @Bean
-  public DataSource dataSource() throws SQLException {
-    if (dbUrl == null || dbUrl.isEmpty()) {
-      return new HikariDataSource();
-    } else {
-      HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
-      return new HikariDataSource(config);
-    }
-  }
 
 }
